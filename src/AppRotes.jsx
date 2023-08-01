85bcc66221887cb4c479daccc9a4a3b9a45927fa
priyanshu negi <priyanshunegi3668@gmail.com>
2023-08-01 16:10:48 +0530

    initial commit to chat app

---
+++ src/AppRotes.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import {BrowserRouters as Router,Rotes,Route} form "react-router-dom";
+export default function AppRotes() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
--- src/index.js
+++ src/index.js
@@ -4,6 +4,24 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+
+
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA-hOJYKU0N0izmmyREspOAN3rtJMoKn7E",
+  authDomain: "chat-app-5861d.firebaseapp.com",
+  projectId: "chat-app-5861d",
+  storageBucket: "chat-app-5861d.appspot.com",
+  messagingSenderId: "717043626630",
+  appId: "1:717043626630:web:fc56160ec159ad2107d707",
+  measurementId: "G-WD5F4NM5WF"
+};
+
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
@@ -11,7 +29,5 @@ root.render(
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+
 reportWebVitals();
